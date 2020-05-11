f09e92252be433f965e50ae765b16b15c4b0e90f
Забозлаев Илья Вячеславович <zabozlaeviv@sovcombank.ru>
2020-05-11 19:52:59 +0300

    not null annotated

--- src/main/java/com/example/trello/dtos/CreateCommentDto.java
+++ src/main/java/com/example/trello/dtos/CreateCommentDto.java
@@ -2,7 +2,10 @@ package com.example.trello.dtos;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 public class CreateCommentDto {
+    @NotNull
     private String content;
 }
